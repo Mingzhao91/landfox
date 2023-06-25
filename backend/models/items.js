b272c17187020513d6af5262e2aa9cc271c4eda4
Mingzhao Zhou <edwardwhite7@hotmail.com>
2023-06-25 20:14:09 +0100

    define type for each fields

--- backend/models/items.js
+++ backend/models/items.js
@@ -1,10 +1,15 @@
 const mongoose = require("mongoose");
 
 const itemSchema = mongoose.Schema({
-  name: String,
-  description: String,
-  price: Number,
-  image: String,
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, rquired: true },
+  image: { type: String, required: true },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Category",
+    required: true,
+  },
 });
 
 Item = mongoose.model("Item", itemSchema);
