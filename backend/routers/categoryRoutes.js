f649101336e401a34e8c2be88e08c7f3f9a87480
Mingzhao Zhou <edwardwhite7@hotmail.com>
2023-06-25 21:18:07 +0100

    define routes for get/update/delete individual category

--- backend/routers/categoryRoutes.js
+++ backend/routers/categoryRoutes.js
@@ -18,6 +18,64 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/:id", async (req, res) => {
+  try {
+    const category = await Category.findById(req.params.id);
+
+    if (!category) {
+      return res.status(400).send("Invalid category Id");
+    }
+
+    return res.status(200).send(category);
+  } catch (err) {
+    res.status(500).json({
+      error: err,
+      message: "Impossible to get this category.",
+    });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const category = await Category.findByIdAndUpdate(
+      req.params.id,
+      {
+        name: req.body.name,
+        categoryType: req.body.categoryType,
+      },
+      { new: true }
+    );
+
+    if (!category) {
+      return res.status(400).send("Invalid category Id");
+    }
+
+    return res.status(200).send(category);
+  } catch (err) {
+    res.status(500).json({
+      error: err,
+      message: "Impossible to update this category.",
+    });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const category = await Category.findByIdAndRemove(req.params.id);
+
+    if (!category) {
+      return res.status(400).send("Invalid category Id");
+    }
+
+    return res.status(200).send("Category deleted successfully.");
+  } catch (err) {
+    res.status(500).json({
+      error: err,
+      message: "Error durring category deletion.",
+    });
+  }
+});
+
 router.post("/", async (req, res) => {
   try {
     const category = new Category({
