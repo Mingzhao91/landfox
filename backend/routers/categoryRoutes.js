364996b63653d777051992e4ca895a1cdb8501f4
Mingzhao Zhou <edwardwhite7@hotmail.com>
2023-06-27 19:51:04 +0100

    fix typo

--- backend/routers/categoryRoutes.js
+++ backend/routers/categoryRoutes.js
@@ -71,7 +71,7 @@ router.delete("/:id", async (req, res) => {
   } catch (err) {
     res.status(500).json({
       error: err,
-      message: "Error durring category deletion.",
+      message: "Error during category deletion.",
     });
   }
 });
