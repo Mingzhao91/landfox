b73d78a84e9ee282e49ae5781302338752e7d56e
Mingzhao Zhou <edwardwhite7@hotmail.com>
2023-06-27 19:23:28 +0100

    save image in post method using multer

--- backend/routers/itemRoutes.js
+++ backend/routers/itemRoutes.js
@@ -1,9 +1,11 @@
 const Item = require("../models/item");
 const express = require("express");
 const router = express.Router();
+const uploads = require("../management/multer");
+const Category = require("../models/category");
 
 router.get("/", async (req, res) => {
-  const items = await Item.find();
+  const items = await Item.find().populate("category");
 
   if (!items) {
     return res.status(400).send("No items found.");
@@ -12,13 +14,29 @@ router.get("/", async (req, res) => {
   return res.status(200).send(items);
 });
 
-router.post("/", async (req, res) => {
+router.post("/", uploads.single("image"), async (req, res) => {
   try {
+    const category = await Category.findById(req.body.category);
+    if (!category) {
+      return res.status(400).send("Unknown category Id.");
+    }
+
+    const file = req.file;
+    if (!file) {
+      return res.status(400).send("Image required!");
+    }
+
+    const fileName = req.file.filename;
+    const path = `${req.protocol}://${req.get("host")}/public/images`;
+
+    console.log("path: ", path);
+
     let item = new Item({
       name: req.body.name,
       description: req.body.description,
       price: req.body.price,
-      image: req.body.image,
+      image: `${path}${fileName}`,
+      category: req.body.category,
     });
 
     await item.save();
