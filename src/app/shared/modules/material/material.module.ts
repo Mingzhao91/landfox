23cb092022dc38d54c80609bdb7a824516e902af
Mingzhao Zhou <edwardwhite7@hotmail.com>
2023-06-29 20:26:50 +0100

    update angular to 15

--- src/app/shared/modules/material/material.module.ts
+++ src/app/shared/modules/material/material.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
+import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
 
 const materialModules = [MatIconModule, MatButtonModule];
 
