aee2735f59d23add668112817abf038db13a7dbe
Mingzhao Zhou <edwardwhite7@hotmail.com>
2023-06-29 20:37:22 +0100

    import MatButtonModule

--- src/app/shared/modules/material/material.module.ts
+++ src/app/shared/modules/material/material.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MatIconModule } from '@angular/material/icon';
-import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
+import { MatButtonModule } from '@angular/material/button';
 
 const materialModules = [MatIconModule, MatButtonModule];
 
