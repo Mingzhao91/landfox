5a15b7433dc6e737d1991a0f7f1088ac9a99a95d
Mingzhao Zhou <edwardwhite7@hotmail.com>
2023-06-29 20:00:50 +0100

    create ground-zero component

---
+++ src/app/shared/components/ground-zero/ground-zero.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ground-zero',
+  templateUrl: './ground-zero.component.html',
+  styleUrls: ['./ground-zero.component.scss']
+})
+export class GroundZeroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
