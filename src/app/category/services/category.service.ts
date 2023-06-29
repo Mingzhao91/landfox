29ddd46e6993c9027f1b2fbf54f8ecd3de96333a
Mingzhao Zhou <edwardwhite7@hotmail.com>
2023-06-29 21:33:19 +0100

    create category service to perform CRUD operations

---
+++ src/app/category/services/category.service.ts
@@ -0,0 +1,39 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+
+import { Observable } from 'rxjs';
+
+import { environment } from 'src/environments/environment';
+
+import { Category } from '../models/category';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CategoryService {
+  http = inject(HttpClient);
+  apiURLCategories = `${environment.apiURL}/category`;
+
+  getCategories(): Observable<Category[]> {
+    return this.http.get<Category[]>(this.apiURLCategories);
+  }
+
+  getCategory(categoryId: string): Observable<Category> {
+    return this.http.get<Category>(`${this.apiURLCategories}/${categoryId}`);
+  }
+
+  createCategory(category: Category): Observable<Category> {
+    return this.http.post<Category>(`${this.apiURLCategories}`, category);
+  }
+
+  updateCategory(category: Category): Observable<Category> {
+    return this.http.put<Category>(
+      `${this.apiURLCategories}/${category._id}`,
+      category
+    );
+  }
+
+  deleteCategory(categoryId: string): Observable<Category> {
+    return this.http.delete<Category>(`${this.apiURLCategories}/${categoryId}`);
+  }
+}
