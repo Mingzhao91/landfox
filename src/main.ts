a54b92f49d5a4cf6ba15120781ab98103bff8339
Mingzhao Zhou <edwardwhite7@hotmail.com>
2023-06-29 22:35:48 +0100

    provide http client

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routs';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes)],
+  providers: [provideHttpClient(), provideRouter(routes)],
 });
