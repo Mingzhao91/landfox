13610ffe2570cd581476032d54b40d8a2b1f7105
Mingzhao Zhou <edwardwhite7@hotmail.com>
2023-06-30 20:22:09 +0100

    dialog for deleting a category

---
+++ src/app/category/components/category-dialog/category-dialog.component.ts
@@ -0,0 +1,25 @@
+import { Component, Inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+
+import {
+  MAT_DIALOG_DATA,
+  MatDialogRef,
+  MatDialogModule,
+} from '@angular/material/dialog';
+
+import { Category } from '../../models/category';
+
+@Component({
+  selector: 'app-category-dialog',
+  standalone: true,
+  imports: [CommonModule, MatButtonModule, MatDialogModule],
+  templateUrl: './category-dialog.component.html',
+  styleUrls: ['./category-dialog.component.scss'],
+})
+export class CategoryDialogComponent {
+  constructor(
+    public dialogRef: MatDialogRef<CategoryDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: Category
+  ) {}
+}
