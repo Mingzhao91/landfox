174affe92a2f68ff30a45c8b24ab5bb5e4e10ded
Mingzhao Zhou <edwardwhite7@hotmail.com>
2023-06-30 20:23:25 +0100

    delete category from table

--- src/app/category/components/category-list/category-list.component.ts
+++ src/app/category/components/category-list/category-list.component.ts
@@ -5,19 +5,30 @@ import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { MatTableModule } from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 
 import { Category } from '../../models/category';
 import { CategoryService } from '../../services/category.service';
 
+import { CategoryDialogComponent } from '../category-dialog/category-dialog.component';
+
 @Component({
   selector: 'app-category-list',
   standalone: true,
-  imports: [CommonModule, MatTableModule, MatIconModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    MatTableModule,
+    MatIconModule,
+    MatButtonModule,
+    MatDialogModule,
+    CategoryDialogComponent,
+  ],
   templateUrl: './category-list.component.html',
   styleUrls: ['./category-list.component.scss'],
 })
 export class CategoryListComponent {
   destroyRef = inject(DestroyRef);
+  dialog = inject(MatDialog);
   categories!: Category[];
   categoryService = inject(CategoryService);
   displayedColumns: string[] = ['name', 'categoryType', 'edit', 'delete'];
@@ -33,5 +44,22 @@ export class CategoryListComponent {
       .subscribe((categories) => {
         this.categories = categories;
       });
+  }
+
+  onDeleteCategory(category: Category) {
+    const dialogRef = this.dialog.open(CategoryDialogComponent, {
+      data: { name: category.name },
+    });
+
+    dialogRef.afterClosed().subscribe((result: boolean) => {
+      if (result) {
+        this.categoryService
+          .deleteCategory(category._id)
+          .pipe(takeUntilDestroyed(this.destroyRef))
+          .subscribe(() => {
+            this.getCategories();
+          });
+      }
+    });
   }
 }
