edd79bb31f65d492bdcc0344eb8836106a61c6c8
mingzhaozhounephos <mz@nephos-solutions.co.uk>
2023-07-01 11:45:56 +0100

    import RouterLink module for navigation

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
@@ -8,7 +9,7 @@ import { MatButtonModule } from '@angular/material/button';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   standalone: true,
-  imports: [MatButtonModule, MatIconModule],
+  imports: [MatButtonModule, MatIconModule, RouterLink],
 })
 export class HeaderComponent implements OnInit {
   constructor() {}
