42050365ca85d4758e0676e0102d16895e4ddde9
mingzhaozhounephos <mz@nephos-solutions.co.uk>
2023-07-01 11:08:18 +0100

    init category form value

--- src/app/category/components/category-form/category-form.component.ts
+++ src/app/category/components/category-form/category-form.component.ts
@@ -1,13 +1,74 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+
+import { CategoryService } from '../../services/category.service';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { filter, map, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-category-form',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatInputModule,
+  ],
   templateUrl: './category-form.component.html',
-  styleUrls: ['./category-form.component.scss']
+  styleUrls: ['./category-form.component.scss'],
 })
-export class CategoryFormComponent {
+export class CategoryFormComponent implements OnInit {
+  formBuilder = inject(FormBuilder);
+  route = inject(ActivatedRoute);
+  destroyRef = inject(DestroyRef);
+  categoryService = inject(CategoryService);
+  isSubmitted = false;
+  editMode = false;
+  currentCategoryId!: string;
 
+  categoryForm!: FormGroup;
+
+  ngOnInit(): void {
+    this.initCategoryForm();
+    this.checkEditMode();
+  }
+
+  initCategoryForm() {
+    this.categoryForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      categoryType: [''],
+    });
+  }
+
+  checkEditMode() {
+    this.route.params
+      .pipe(
+        filter((params) => params['id']),
+        switchMap(({ id }) => {
+          this.editMode = true;
+          this.currentCategoryId = id;
+          return this.categoryService.getCategory(id);
+        }),
+        takeUntilDestroyed(this.destroyRef)
+      )
+      .subscribe((category) => {
+        this.categoryForm.patchValue({
+          name: category.name,
+          categoryType: category.categoryType,
+        });
+      });
+  }
 }
