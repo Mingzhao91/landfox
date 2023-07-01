bd1be55e8fc10b742373885564ac4fd13295f91a
mingzhaozhounephos <mz@nephos-solutions.co.uk>
2023-07-01 19:40:21 +0100

    filter items by category and name

--- src/app/item/components/item-list/item-list.component.ts
+++ src/app/item/components/item-list/item-list.component.ts
@@ -1,5 +1,11 @@
 import { Component, DestroyRef, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 import { MatCardModule } from '@angular/material/card';
@@ -7,6 +13,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
 
 import { Item } from '../../models/item.interface';
 import { Category } from '../../../category/models/category.interface';
@@ -19,28 +26,38 @@ import { CategoryService } from '../../../category/services/category.service';
   standalone: true,
   imports: [
     CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
     MatCardModule,
     MatIconModule,
     MatButtonModule,
     MatFormFieldModule,
     MatSelectModule,
+    MatInputModule,
   ],
   templateUrl: './item-list.component.html',
   styleUrls: ['./item-list.component.scss'],
 })
 export class ItemListComponent implements OnInit {
+  formBuilder = inject(FormBuilder);
   destroyRef = inject(DestroyRef);
   itemService = inject(ItemService);
   categoryService = inject(CategoryService);
 
   items!: Item[];
   categories!: Category[];
-  filter!: string;
+  filterByCategory: string = '';
   showAll = true;
+
+  searchForm!: FormGroup;
+  filterByName: string = '';
 
   ngOnInit(): void {
     this.getItems();
     this.getCategories();
+    this.searchForm = this.formBuilder.group({
+      term: [''],
+    });
   }
 
   getItems() {
@@ -62,8 +79,31 @@ export class ItemListComponent implements OnInit {
   }
 
   filterItems(value: string) {
-    this.filter = value;
-    this.showAll = this.filter === 'All';
+    this.filterByCategory = value;
+  }
+
+  onSubmit() {
+    this.filterByName = this.searchForm.get('term')?.value.trim();
+  }
+
+  showItem(item: Item) {
+    let isShown = false;
+    let shownByCategory = false;
+    let shownBySearch = false;
+
+    shownByCategory =
+      this.filterByCategory === 'All' ||
+      item.category.name === this.filterByCategory ||
+      this.filterByCategory === '';
+
+    shownBySearch =
+      (this.filterByName.length > 0 &&
+        item.name.toLocaleLowerCase().includes(this.filterByName)) ||
+      this.filterByName.length === 0;
+
+    isShown = shownByCategory && shownBySearch;
+
+    return isShown;
   }
 
   forwardToSingleItem(itemId: string) {}
