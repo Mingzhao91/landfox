6e9e968241eda23ef7be6123b0615def06a83bf7
mingzhaozhounephos <mz@nephos-solutions.co.uk>
2023-07-02 13:29:02 +0100

    form for creating and updating item

--- src/app/item/components/item-form/item-form.component.ts
+++ src/app/item/components/item-form/item-form.component.ts
@@ -26,6 +26,8 @@ import { MatSelectModule } from '@angular/material/select';
 import { filter, switchMap, take } from 'rxjs';
 
 import { Category } from 'src/app/category/models/category.interface';
+import { ItemService } from '../../services/item.service';
+import { CategoryService } from 'src/app/category/services/category.service';
 
 @Component({
   selector: 'app-item-form',
@@ -47,25 +49,45 @@ import { Category } from 'src/app/category/models/category.interface';
 export class ItemFormComponent implements OnInit {
   formBuilder = inject(FormBuilder);
   _ngZone = inject(NgZone);
+  router = inject(Router);
+  route = inject(ActivatedRoute);
+  destroyRef = inject(DestroyRef);
+  itemService = inject(ItemService);
+  categoryService = inject(CategoryService);
 
+  snackBar = inject(MatSnackBar);
   itemForm!: FormGroup;
   categories!: Category[];
   imageDisplay!: string | ArrayBuffer;
+  editMode = false;
+  currentItemId!: string;
+  isSubmitted = false;
 
   @ViewChild('autosize') autosize!: CdkTextareaAutosize;
 
   ngOnInit(): void {
+    this.getCategories();
     this.initItemForm();
+    this.checkEditMode();
   }
 
   initItemForm() {
     this.itemForm = this.formBuilder.group({
       name: ['', Validators.required],
       description: ['', Validators.required],
-      price: [0, Validators.required],
-      image: [null],
+      price: ['', Validators.required],
+      image: [''],
       category: ['', Validators.required],
     });
+  }
+
+  getCategories() {
+    this.categoryService
+      .getCategories()
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe((categories) => {
+        this.categories = categories;
+      });
   }
 
   triggerResize() {
@@ -90,5 +112,75 @@ export class ItemFormComponent implements OnInit {
     }
   }
 
-  onSubmit() {}
+  checkEditMode() {
+    this.route.params
+      .pipe(
+        filter((params) => params['id']),
+        switchMap(({ id }) => {
+          this.editMode = true;
+          this.currentItemId = id;
+          return this.itemService.getItem(id);
+        }),
+        takeUntilDestroyed(this.destroyRef)
+      )
+      .subscribe((item) => {
+        this.imageDisplay = item.image;
+
+        this.itemForm.patchValue({
+          name: item.name,
+          description: item.description,
+          price: item.price,
+          category: item.category._id,
+        });
+      });
+  }
+
+  openSnackBar(message: string, action: string) {
+    this.snackBar.open(message, action, {
+      horizontalPosition: 'right',
+      verticalPosition: 'top',
+      duration: 4000,
+    });
+  }
+
+  addItem(item: FormData) {
+    this.itemService
+      .createItem(item)
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe(async (newItem) => {
+        this.isSubmitted = false;
+        const isNavigated = await this.router.navigate(['/items/list']);
+
+        if (isNavigated) {
+          this.openSnackBar(`You added ${newItem.name} as new item`, 'Okay');
+        }
+      });
+  }
+
+  updateItem(item: FormData) {
+    this.itemService
+      .updateItem(item, this.currentItemId)
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe(async (newItem) => {
+        this.isSubmitted = false;
+        const isNavigated = await this.router.navigate(['/items/list']);
+
+        if (isNavigated) {
+          this.openSnackBar(`You updated ${newItem.name} item`, 'Okay');
+        }
+      });
+  }
+
+  onSubmit() {
+    this.isSubmitted = true;
+
+    const itemData = new FormData();
+    itemData.append('name', this.itemForm.get('name')?.value);
+    itemData.append('description', this.itemForm.get('description')?.value);
+    itemData.append('price', this.itemForm.get('price')?.value);
+    itemData.append('image', this.itemForm.get('image')?.value);
+    itemData.append('category', this.itemForm.get('category')?.value);
+
+    this.editMode ? this.updateItem(itemData) : this.addItem(itemData);
+  }
 }
