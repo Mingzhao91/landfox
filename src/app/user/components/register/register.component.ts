fa6b7b2b5c4eac2f49ed896985294d666063856c
mingzhaozhounephos <mz@nephos-solutions.co.uk>
2023-07-02 21:27:25 +0100

    register user

---
+++ src/app/user/components/register/register.component.ts
@@ -0,0 +1,76 @@
+import { Component, OnInit, inject, DestroyRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { Router, RouterLink, RouterModule } from '@angular/router';
+
+import { AuthService } from '../../services/auth.service';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { User } from '../../models/user.interface';
+import { LocalStorageService } from 'src/app/shared/services/local-storage.service';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    RouterModule,
+    RouterLink,
+  ],
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss'],
+})
+export class RegisterComponent implements OnInit {
+  formBuilder = inject(FormBuilder);
+  router = inject(Router);
+  destroyRef = inject(DestroyRef);
+  authService = inject(AuthService);
+  localStorageService = inject(LocalStorageService);
+
+  signupForm!: FormGroup;
+  isSubmitted = false;
+  authError = false;
+
+  ngOnInit(): void {
+    this.initSignupForm();
+  }
+
+  initSignupForm() {
+    this.signupForm = this.formBuilder.group({
+      email: ['', [Validators.email, Validators.required]],
+      password: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    this.isSubmitted = true;
+    this.authService
+      .signup(
+        this.signupForm.get('email')?.value,
+        this.signupForm.get('password')?.value
+      )
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe({
+        next: () => {
+          this.router.navigateByUrl('/users/login');
+        },
+        error: (err) => {
+          this.authError = err;
+        },
+      });
+  }
+}
