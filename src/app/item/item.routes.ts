389411b5a9d88132bb73e2a0229d9259e7591c18
mingzhaozhounephos <mz@nephos-solutions.co.uk>
2023-07-03 21:49:51 +0100

    use auth guard to protect item creation and edtion

--- src/app/item/item.routes.ts
+++ src/app/item/item.routes.ts
@@ -4,6 +4,7 @@ import { ItemListComponent } from './components/item-list/item-list.component';
 import { ItemListAdminComponent } from './components/item-list-admin/item-list-admin.component';
 import { ItemFormComponent } from './components/item-form/item-form.component';
 import { SingleItemComponent } from './components/single-item/single-item.component';
+import { authGuard } from '../shared/services/auth.guard';
 
 export const routes: Routes = [
   {
@@ -17,10 +18,12 @@ export const routes: Routes = [
   {
     path: 'form',
     component: ItemFormComponent,
+    canActivate: [authGuard],
   },
   {
     path: 'form/:id',
     component: ItemFormComponent,
+    canActivate: [authGuard],
   },
   {
     path: 'single-item/:id',
