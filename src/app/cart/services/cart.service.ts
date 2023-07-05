396cb5ed44339cc4a1ff79196c16f3a6c84b8a1f
mingzhaozhounephos <mz@nephos-solutions.co.uk>
2023-07-05 21:42:18 +0100

    create service to manage cart

---
+++ src/app/cart/services/cart.service.ts
@@ -0,0 +1,71 @@
+import { Injectable, inject } from '@angular/core';
+
+import { Cart, CartItem } from '../models/cart.interface';
+import { LocalStorageService } from '../../shared/services/local-storage.service';
+import { BehaviorSubject } from 'rxjs';
+
+const CART = 'cart';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CartService {
+  cart$: BehaviorSubject<Cart> = new BehaviorSubject(this.getCart());
+  localStorageService = inject(LocalStorageService);
+
+  initialCartLocalStorage() {
+    const cart: Cart = this.getCart();
+
+    if (!cart) {
+      const initialCart = {
+        cartItem: [],
+        user: {},
+      };
+      const initialCartJson = JSON.stringify(initialCart);
+      localStorage.setItem(CART, initialCartJson);
+    }
+  }
+
+  getCart(): Cart {
+    const cartJsonString = localStorage.getItem(CART);
+    return cartJsonString ? JSON.parse(cartJsonString) : null;
+  }
+
+  setCartItem(cartItem: CartItem) {
+    const cart = this.getCart();
+    const cartItemExist = cart.cartItems.find(
+      (item) => item.item === cartItem.item
+    );
+
+    if (cartItemExist) {
+      cart.cartItems.map((item) => {
+        if (item.item === cartItem.item) {
+          item.quantity = item.quantity + cartItem.quantity;
+        }
+      });
+    } else {
+      const user = this.localStorageService.getUserId();
+
+      if (user) {
+        cart.cartItems.push(cartItem);
+        cart.user = user;
+      }
+    }
+
+    this.storeAndEmitCart(cart);
+
+    return cart;
+  }
+
+  removeItem(index: number) {
+    const cart = this.getCart();
+    cart.cartItems.splice(index, 1);
+    this.storeAndEmitCart(cart);
+  }
+
+  storeAndEmitCart(cart: Cart) {
+    const cartJson = JSON.stringify(cart);
+    localStorage.setItem(CART, cartJson);
+    this.cart$.next(cart);
+  }
+}
