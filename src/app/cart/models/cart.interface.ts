a3cc820327fbf5b2a4a733f7d439fc6bef89b0bc
mingzhaozhounephos <mz@nephos-solutions.co.uk>
2023-07-05 21:41:49 +0100

    create interfaces about cart

---
+++ src/app/cart/models/cart.interface.ts
@@ -0,0 +1,21 @@
+import { Item } from '../../item/models/item.interface';
+
+export interface Cart {
+  _id: string;
+  cartItems: CartItem[];
+  user?: string;
+  totalPrice: number;
+}
+
+export interface CartItem {
+  _id: string;
+  item: Item;
+  quantity: number;
+  user?: string;
+}
+
+export interface CartItemDetailed {
+  _id: string;
+  item: Item;
+  quantity: number;
+}
