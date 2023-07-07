d81a5c0819f63ed951019b118532c75b7ba4e030
mingzhaozhounephos <mz@nephos-solutions.co.uk>
2023-07-07 20:35:59 +0100

    show cart item cout in header

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,18 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DestroyRef, OnInit, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatBadgeModule } from '@angular/material/badge';
+
+import { CartService } from '../../../cart/services/cart.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   standalone: true,
-  imports: [MatButtonModule, MatIconModule, RouterLink],
+  imports: [RouterLink, MatButtonModule, MatIconModule, MatBadgeModule],
 })
 export class HeaderComponent implements OnInit {
-  constructor() {}
+  cartService = inject(CartService);
+  destroyRef = inject(DestroyRef);
 
-  ngOnInit(): void {}
+  cartCount = 0;
+
+  ngOnInit(): void {
+    this.cartService.cart$
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe((cart) => {
+        console.log('cart: ', cart);
+        this.cartCount = cart.cartItems.length ?? 0;
+      });
+  }
 }
