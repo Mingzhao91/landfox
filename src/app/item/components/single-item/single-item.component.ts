b086ec39112bf4c503cb198a0c7c89ca17e3244c
mingzhaozhounephos <mz@nephos-solutions.co.uk>
2023-07-07 20:35:20 +0100

    add item into cart

--- src/app/item/components/single-item/single-item.component.ts
+++ src/app/item/components/single-item/single-item.component.ts
@@ -1,21 +1,24 @@
 import { Component, inject, DestroyRef, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { ActivatedRoute, Router } from '@angular/router';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 import { of, switchMap } from 'rxjs';
 
-import { Item } from '../../models/item.interface';
-import { ItemService } from '../../services/item.service';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+
+import { Item } from '../../models/item.interface';
+import { ItemService } from '../../services/item.service';
+
+import { CartItem } from '../../../cart/models/cart.interface';
+import { CartService } from '../../../cart/services/cart.service';
 
 @Component({
   selector: 'app-single-item',
   standalone: true,
-  imports: [CommonModule, MatButtonModule, MatIconModule],
+  imports: [CommonModule, MatButtonModule, MatIconModule, MatSnackBarModule],
   templateUrl: './single-item.component.html',
   styleUrls: ['./single-item.component.scss'],
 })
@@ -23,7 +26,9 @@ export class SingleItemComponent implements OnInit {
   route = inject(ActivatedRoute);
   router = inject(Router);
   destroyRef = inject(DestroyRef);
+  snackBar = inject(MatSnackBar);
   itemService = inject(ItemService);
+  cartService = inject(CartService);
 
   item!: Item;
 
@@ -45,9 +50,26 @@ export class SingleItemComponent implements OnInit {
       });
   }
 
-  addToCart(item: Item) {}
+  addToCart(item: Item) {
+    const cartItem: CartItem = {
+      _id: item._id,
+      item: item,
+      quantity: 1,
+    };
+
+    this.cartService.setCartItem(cartItem);
+    this.openSnackBar(`You added ${cartItem.item.name} to the cart`, 'Okay');
+  }
 
   backToShop() {
     this.router.navigateByUrl('/');
+  }
+
+  openSnackBar(message: string, action: string) {
+    this.snackBar.open(message, action, {
+      horizontalPosition: 'right',
+      verticalPosition: 'top',
+      duration: 4000,
+    });
   }
 }
